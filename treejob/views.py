f4439c4f0f150cdbb2a70d81732e22041f49e53e
corey james <coreyjames689@aol.com>
2024-06-20 21:34:40 -0600

    template added

--- treejob/views.py
+++ treejob/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+
+from django.template import loader
 
 # Create your views here.
 
 def trees(request):
-    return HttpResponse('Hello World!')
+    template = loader.get_template('first.html')
+    return HttpResponse(template.render())
