b4d2d622ecd489ed7baedb8c3a9b6f7960ab198f
corey james <coreyjames689@aol.com>
2024-06-21 20:13:49 -0600

    add record to treejob db

--- treejob/models.py
+++ treejob/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Tree(models.Model):
+    firsttreebusiness = models.CharField(max_length=255)
+    secondtreebusiness = models.CharField(max_length=255)
